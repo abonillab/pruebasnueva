1f6373bdb27088ecb62f180d1fe6605a55635aba
Adriana Paola Bonilla <Adriana Paola Bonilla>
2017-04-06 00:18:31 -0500

    form

--- src/main/webapp/resources/js/datos-basicos/datosbasicos.controller.js
+++ src/main/webapp/resources/js/datos-basicos/datosbasicos.controller.js
@@ -6,7 +6,12 @@
 	DatosBasicosController.$inject = [ '$scope' ];
 
 	function DatosBasicosController($scope) {
+		this.myDate = new Date();
 
+		this.minDate = new Date(1979, 12, 1);
+
+		this.maxDate = new Date(this.myDate.getFullYear() - 7, this.myDate
+				.getMonth(), this.myDate.getDate());
 
 	}
 })();
